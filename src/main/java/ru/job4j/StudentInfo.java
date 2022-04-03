1ac5ce1184e44e5a14956ad9c34492d37ff53728
giantmidget182 <giantmidget182@gmail.com>
2022-04-03 15:19:33 +0700

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+System.out.println("Aleksandr Kozlov");
+        System.out.println("22.09.1990");
+    }
+}
+
