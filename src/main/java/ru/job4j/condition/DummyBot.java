31a3596656320476be0710c855a71b898610463c
giantmidget182 <giantmidget182@gmail.com>
2022-04-26 22:44:39 +0700

    Глупый бот [#31323 #293748]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,7 +4,7 @@ public class DummyBot {
 
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
-        if ("Привет, Бот".equals(question)) {
+        if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник";
         } else if ("Пока".equals(question)) {
             rsl = "До скорой встречи";
@@ -13,7 +13,7 @@ public class DummyBot {
     }
 
     public static void main(String[] args) {
-        String rsl = DummyBot.answer("Привет, Бот");
+        String rsl = DummyBot.answer("Привет, Бот.");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока");
         System.out.println(rsl);
