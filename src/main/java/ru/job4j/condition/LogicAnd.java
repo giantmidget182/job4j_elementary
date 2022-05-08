babdc823a5c3e61b23e0673e29690d104502a28c
giantmidget182 <giantmidget182@gmail.com>
2022-05-08 17:20:53 +0700

    Построить шахматную доску в псевдографике. [#13559]

--- src/main/java/ru/job4j/condition/LogicAnd.java
+++ src/main/java/ru/job4j/condition/LogicAnd.java
@@ -4,7 +4,7 @@ public class LogicAnd {
     public static boolean check(int num) {
         return num > 0 && num % 2 == 0;
     }
-    
+
         public static void main(String[] args) {
            boolean result = LogicAnd.check(5);
             System.out.println(result);
