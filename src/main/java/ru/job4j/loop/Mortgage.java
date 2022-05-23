1ab8cb345b4d26bc0a97eff9d9c830d6ba3098f7
giantmidget182 <giantmidget182@gmail.com>
2022-05-23 17:24:44 +0700

    Ипотека [#156315 #299631]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double pay = amount + (amount * percent / 100);
-        while (pay >= 0) {
-            pay = pay - salary;
+        while (amount + (amount * percent / 100) >= 0) {
+            amount = amount + (amount * percent / 100) - salary;
             year++;
+
         }
         return year;
     }
